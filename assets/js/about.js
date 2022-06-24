86f4889b49accd2c32b9caf9e537b4a61f1d8734
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-06-24 18:33:36 -0300

    Fix Linters

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,8 +1,8 @@
 /*  ------=========     Menu    =========------  */
 
 function mobileMenu() {
- document.querySelector('HEADER').classList.toggle('header-menu');
- document.querySelector('.headerLogo').classList.toggle('hide');
+  document.querySelector('HEADER').classList.toggle('header-menu');
+  document.querySelector('.headerLogo').classList.toggle('hide');
 }
 
 document.querySelector('#menuIcon').addEventListener('click', () => {
